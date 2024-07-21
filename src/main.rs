e67d9d5e9651ce307815119d4a263ff2489000e3
Jan Żukowski <janzukowski@proton.me>
2024-07-21 20:22:12 +0200

    update: added Config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+use clap::Parser;
 use simple_logger::SimpleLogger;
+
+#[derive(Parser, Debug, Clone)]
+#[command(version, about = "A simple HTTP server.")]
+struct CLIConfig {
+    #[arg(short, long, default_value = "8080", help = "The port that server will be listening for requests on.")]
+    port: u16,
+    #[arg(short, long, default_value = ".", help = "The directory that will be served.")]
+    directory: String,
+    #[arg(short, long, default_value = "index.html", help = "The file that will be read from requested path when user requests url ending with '/'.")]
+    index: String
+}
 
 
 fn main() {
     SimpleLogger::new().init();
+
+    let cli_config = CLIConfig::parse();
 }
